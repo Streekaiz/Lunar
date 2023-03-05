a950ab373b09271e661e20ba3a76f60c20052507
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2023-03-05 09:30:02 -0800

    Create Loader.lua

---
+++ Modules/Loader.lua
@@ -0,0 +1,2 @@
+getgenv().Utility = loadstring(game:HttpGet("https://raw.githubusercontent.com/Streekaiz/Lunar/main/Modules/Utility.lua", true))()
+getgenv().Math = loadstring(game:HttpGet("https://raw.githubusercontent.com/Streekaiz/Lunar/main/Modules/Math.lua", true))()
