618478e0a5eae6b15f7e6b591ba442c4a769e69d
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2024-06-29 18:03:18 -0700

    Update Library.lua

--- comps/unminified/Library.lua
+++ comps/unminified/Library.lua
@@ -1,156 +1,136 @@
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua'))()
+local ThemeManager = loadstring(game:HttpGet('https://raw.githubusercontent.com/Streekaiz/linoria-fork/main/addons/ThemeManager.lua'))()
+local SaveManager = loadstring(game:HttpGet('https://raw.githubusercontent.com/Streekaiz/linoria-fork/main/addons/SaveManager.lua'))()
 
-
-local Fluent, SaveManager, InterfaceManager = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))(), loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))(), loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-
-local Window = Fluent:CreateWindow({
-    Title = "lunar",
-    SubTitle = " .gg/lunar",
-    TabWidth = 160,
-    Size = UDim2.fromOffset(580, 460),
-    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark",
-    MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
+local Window = Library:CreateWindow({
+    Title = 'Lunar | Jailbird | dev',
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.2
 })
 
+--// Setting up Tabs
+
+Window:AddTab("Combat")
+Window:AddTab("Visual")
+Window:AddTab("Player")
+Window:AddTab("Miscallaenous")
+Window:AddTab("Settings")
+
+--// Combat
+
+local Hitbox = Window.Tabs.Combat:AddLeftGroupbox('Hitbox Expander') do
+	Hitbox:AddToggle('hitbox_enabled', { Text = 'Enabled', Tooltip = "Expands other enemies hitboxes.", Risky = true }):AddKeyPicker('hitbox_key', {SyncToggleState = true, Mode = 'Toggle', Default = "PageUp", Text = 'Hitbox Expander', NoUI = false})
+
+	local Depbox = Hitbox:AddDependencyBox()
+
+	Depbox:AddDropdown('hitbox_part', {
+		Values = {"Head", "Body"}, Default = 1,
+	
+		Text = 'Hitbox Target',
+		Tooltip = 'Expands the selected hitbox',
+	})
+	
+	Depbox:AddSlider('hitbox_amount', {
+		Text = 'Expansion Value', Default = 3, Min = 0, Max = 10, Rounding = 2, Tooltip = "Affects how expanded the hitbox is."
+	})
+
+	Depbox:AddToggle('hitbox_ffa', { Text = 'FFA Mode', Tooltip = "Expand other players hitboxes.", Risky = false });
 
 
-local Tabs = {
-    Home = Window:AddTab({ Title = "Home", Icon = "" }),
-    Player = Window:AddTab({ Title = "Player", Icon = ""}),
-    Combat = Window:AddTab({ Title = "Combat", Icon = "" })
-    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
-}
+	Depbox:AddInput('hitbox_ignore', {
+		Default = game:GetService("Players").LocalPlayer.Name, Numeric = false, Finished = true,
+		
+		Text = 'Ignore Player',
+		Tooltip = 'Ignores the selected player\nInput the username again to remove them.', -- Information shown when you hover over the textbox
+	
+		Placeholder = 'username'
+	})
 
-Home:AddSection("Discord") do
-    Tabs.Home:AddButton({
-        Title = "Copy discord link",
-        Description = "",
-        Callback = function()
-            setclipboard()
-        end
-    })
-    Tabs.Home:AddParagraph({
-        Title = "Information",
-        Content = "Get notified on updates & new tools"
-    })
+	Depbox:SetupDependencies({
+		{ Toggles.hitbox_enabled, true }
+	})
 end
 
-Home:AddSection("Changelogs") do
-    Tabs.Home:AddParagraph({
-        Title = "Update 1.1",
-        Content = "• Debug Logs\n• Hitbox Expansion\n• Server Extension"
-    })
+--// Visuals
 
-    Tabs.Home:AddParagraph({
-        Title = "Planned Updates",
-        Content = "• ESP"
-    })
-end
+local ESP = Window.Tabs.Visual:AddLeftTabbox() do
+	local Enemy, Friendly = ESP:AddTab("Enemy"), ESP:AddTab("Friendly")	
+	Enemy:AddSlider('esp_text_size', { Text = 'Text Size', Default = 13, Min = 0, Max = 30, Rounding = 2, Callback = function(v) Sense.sharedSettings.textSize = v; end})
+	Enemy:AddSlider('esp_distance', { Text = 'Maximum Distance', Default = 5000, Min = 0, Max = 10000, Rounding = 2, Callback = function(v) Sense.sharedSettings.maxDistance = v; Sense.sharedSettings.limitDistance = true end})
+	Enemy:AddDropdown('esp_text_font', { Values = { "1", "2", "3", "4" }, Default = 2, Multi = false, Text = 'Text Font', Tooltip = 'Changes the text font', Callback = function(v) Sense.sharedSettings.textFont = v; end})
+	Enemy:AddDivider()
+	
+	Enemy:AddToggle('esp_enemy_enabled', { Text = 'Enabled', Default = false, Tooltip = 'Enables ESP for enemy players.', Callback = function(v) Sense.teamSettings.enemy.enabled = v end})
+	Enemy:AddToggle('esp_enemy_box', { Text = 'Boxes', Default = false, Tooltip = 'Enables boxes for enemy players.', Callback = function() end}):AddColorPicker('esp_enemy_box_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Box Color', Transparency = 0, Callback = function() end}):AddColorPicker('esp_enemy_boxoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Box Outline Color', Transparency = 0 })
+	Enemy:AddToggle('esp_enemy_boxfill', { Text = 'Box Fillings', Default = false, Tooltip = 'Enables box fillings for enemy players.' }):AddColorPicker('esp_enemy_boxfill_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Box Fill Color', Transparency = 0.5 })
+	Enemy:AddToggle('esp_enemy_health', { Text = 'Health Bars', Default = false, Tooltip = 'Enables health bars for enemy players.' }):AddColorPicker('esp_enemy_healthhealthy_color', { Default = Color3.fromRGB(0, 255, 0), Title = 'Health Bar Healthy Color', Transparency = nil }):AddColorPicker('esp_enemy_healthdying_color', { Default = Color3.fromRGB(255, 0, 0), Title = 'Health Bar Dying Color', Transparency = nil }):AddColorPicker('esp_enemy_healthoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Health Bar Outline Color', Transparency = 0 })
+	Enemy:AddToggle('esp_enemy_name', { Text = 'Names', Default = false, Tooltip = 'Enables names for enemy players.' }):AddColorPicker('esp_enemy_name_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Name Color', Transparency = 0 }):AddColorPicker('esp_enemy_nameoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Name Outline Color', Transparency = 0 })
+	Enemy:AddToggle('esp_enemy_distance', { Text = 'Distances', Default = false, Tooltip = 'Enables distances for enemy players.' }):AddColorPicker('esp_enemy_distance_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Distance Color', Transparency = 0 }):AddColorPicker('esp_enemy_distanceoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Distance Outline Color', Transparency = 0 })
+	Enemy:AddToggle('esp_enemy_healthtext', { Text = 'Health', Default = false, Tooltip = 'Enables health text for enemy players.' }):AddColorPicker('esp_enemy_healthtext_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Health Text Color', Transparency = 0 }):AddColorPicker('esp_enemy_healthtextoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Health Text Outline Color', Transparency = 0 })
+	Enemy:AddToggle('esp_enemy_weapon', { Text = 'Weapons', Default = false, Tooltip = 'Enables weapons for enemy players.' }):AddColorPicker('esp_enemy_weapon_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Weapons Color', Transparency = 0 }):AddColorPicker('esp_enemy_weaponoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Weapon Outline Color', Transparency = 0 })
+	Enemy:AddToggle('esp_enemy_chams', { Text = 'Chams', Default = false, Tooltip = 'Enables chams for enemy players.' }):AddColorPicker('esp_enemy_chams_color', { Default = Color3.fromRGB(0, 200, 0), Title = 'Chams Fill Color', Transparency = 0 }):AddColorPicker('esp_enemy_chamsoutline_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Chams Outline Color', Transparency = 0 })
+	Enemy:AddToggle('esp_enemy_arrows', { Text = 'Arrows', Default = false, Tooltip = 'Enables off-screen arrows for enemy players.' }):AddColorPicker('esp_enemy_arrow_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Arrow Color', Transparency = 0 }):AddColorPicker('esp_enemy_arrowoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Arrow Outline Color', Transparency = 0 })
+	Enemy:AddDivider()
+	Enemy:AddSlider('esp_enemy_arrows_radius', { Text = 'Arrow Radius', Default = 150, Min = 0, Max = 300, Rounding = 2})
+	Enemy:AddSlider('esp_enemy_arrows_size', { Text = 'Arrow Size', Default = 15, Min = 0, Max = 30, Rounding = 2})
+	Enemy:AddDropdown('esp_enemy_chams_type', { Values = { 'AlwaysOnTop', "Occluded" }, Default = 1, Multi = false, Text = 'Chams Type', Tooltip = 'Changes how you see enemy chams.' })
 
-Home:AddSection("Server") do
-    Tabs.Home:AddButton({
-        Title = "Rejoin",
-        Description = "Re-joins your server.",
-        Callback = function()
-            if game:GetService("Players"):GetPlayers() <= 1 then
-                game:GetService("Players").LocalPlayer:Kick("\nRejoining...")
-                task.wait(0.5)
-                TeleportService:Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
-            else
-                TeleportService:TeleportToPlaceInstance(game.PlaceId, game.JobId, game:GetService("Players").LocalPlayer)
-            end        
-        end
-    })
-    Tabs.Home:AddButton({
-        Title = "Server-hop",
-        Description = "Puts you in a different server.",
-        Callback = function()
-            local servers = {}
-            local req = http_request({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true", game.PlaceId)})
-            local body = game:GetService("HttpService"):JSONDecode(req.Body)
-            if body and body.data then
-                for i, v in next, body.data do
-                    if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
-                        table.insert(servers, 1, v.id)
-                    end
-                end
-            end
-            if #servers > 0 then
-                TeleportService:TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], game:GetService("Players").LocalPlayer)
-            else
-                Window:Dialog({
-                    Title = "Error",
-                    Content = "We couldn't find a server.",
-                    Buttons = {
-                        {
-                            Title = "Okay",
-                            Callback = function() end
-                        },
-                        {
-                            Title = "Retry",
-                            Callback = function()
-                                local servers = {}
-                                local req = http_request({Url = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true", game.PlaceId)})
-                                local body = game:GetService("HttpService"):JSONDecode(req.Body)
-                                if body and body.data then
-                                    for i, v in next, body.data do
-                                        if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
-                                            table.insert(servers, 1, v.id)
-                                        end
-                                    end
-                                end
-                                if #servers > 0 then
-                                    TeleportService:TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], game:GetService("Players").LocalPlayer)
-                                end
-                            end
-                        }
-                    }
-                })
-            end
-        end
-    })
-    Tabs.Home:AddInput("server_input", {
-        Title = "Join Player",
-        Default = "",
-        Placeholder = "username / id",
-        Numeric = false, -- Only allows numbers
-        Finished = true, -- Only calls callback when you press enter
-        Callback = function(Value)
-            --dolater
-        end
-    })
+	Friendly:AddToggle('esp_friendly_enabled', { Text = 'Enabled', Default = false, Tooltip = 'Enables ESP for friendly players.' })
+	Friendly:AddToggle('esp_friendly_box', { Text = 'Boxes', Default = false, Tooltip = 'Enables boxes for friendly players.' }):AddColorPicker('esp_friendly_box_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Box Color', Transparency = 0 }):AddColorPicker('esp_friendly_boxoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Box Outline Color', Transparency = 0 })
+	Friendly:AddToggle('esp_friendly_boxfill', { Text = 'Box Fillings', Default = false, Tooltip = 'Enables box fillings for friendly players.' }):AddColorPicker('esp_friendly_boxfill_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Box Fill Color', Transparency = 0.5 })
+	Friendly:AddToggle('esp_friendly_health', { Text = 'Health Bars', Default = false, Tooltip = 'Enables health bars for friendly players.' }):AddColorPicker('esp_friendly_healthhealthy_color', { Default = Color3.fromRGB(0, 255, 0), Title = 'Health Bar Healthy Color', Transparency = nil }):AddColorPicker('esp_friendly_healthdying_color', { Default = Color3.fromRGB(255, 0, 0), Title = 'Health Bar Dying Color', Transparency = nil }):AddColorPicker('esp_friendly_healthoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Health Bar Outline Color', Transparency = 0 })
+	Friendly:AddToggle('esp_friendly_name', { Text = 'Names', Default = false, Tooltip = 'Enables names for friendly players.' }):AddColorPicker('esp_friendly_name_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Name Color', Transparency = 0 }):AddColorPicker('esp_friendly_nameoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Name Outline Color', Transparency = 0 })
+	Friendly:AddToggle('esp_friendly_distance', { Text = 'Distances', Default = false, Tooltip = 'Enables distances for friendly players.' }):AddColorPicker('esp_friendly_distance_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Distance Color', Transparency = 0 }):AddColorPicker('esp_friendly_distanceoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Distance Outline Color', Transparency = 0 })
+	Friendly:AddToggle('esp_friendly_healthtext', { Text = 'Health', Default = false, Tooltip = 'Enables health text for friendly players.' }):AddColorPicker('esp_friendly_healthtext_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Health Text Color', Transparency = 0 }):AddColorPicker('esp_friendly_healthtextoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Health Text Outline Color', Transparency = 0 })
+	Friendly:AddToggle('esp_friendly_weapon', { Text = 'Weapons', Default = false, Tooltip = 'Enables weapons for friendly players.' }):AddColorPicker('esp_friendly_weapon_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Weapons Color', Transparency = 0 }):AddColorPicker('esp_friendly_weaponoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Weapon Outline Color', Transparency = 0 })
+	Friendly:AddToggle('esp_friendly_chams', { Text = 'Chams', Default = false, Tooltip = 'Enables chams for friendly players.' }):AddColorPicker('esp_friendly_chams_color', { Default = Color3.fromRGB(0, 200, 0), Title = 'Chams Fill Color', Transparency = 0 }):AddColorPicker('esp_friendly_chamsoutline_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Chams Outline Color', Transparency = 0 })
+	Friendly:AddToggle('esp_friendly_arrows', { Text = 'Arrows', Default = false, Tooltip = 'Enables off-screen arrows for friendly players.' }):AddColorPicker('esp_friendly_arrow_color', { Default = Color3.fromRGB(255, 255, 255), Title = 'Arrow Color', Transparency = 0 }):AddColorPicker('esp_friendly_arrowoutline_color', { Default = Color3.fromRGB(0, 0, 0), Title = 'Arrow Outline Color', Transparency = 0 })
+	Friendly:AddDivider()
+	Friendly:AddSlider('esp_friendly_arrows_radius', { Text = 'Arrow Radius', Default = 150, Min = 0, Max = 300, Rounding = 2})
+	Friendly:AddSlider('esp_friendly_arrows_size', { Text = 'Arrow Size', Default = 15, Min = 0, Max = 30, Rounding = 2})
+	Friendly:AddDropdown('esp_friendly_chams_type', { Values = { 'AlwaysOnTop', "Occluded" }, Default = 1, Multi = false, Text = 'Chams Type', Tooltip = 'Changes how you see friendly chams.' })
 
 end
 
-Tabs.Player:AddSection("Movement") do
-    Tabs.Player:AddDropdown("player_method", {
-        Title = "Movement Method",
-        Values = {"CFrame", "Regular"},
-        Multi = false,
-        Default = 2,
-    })
+--// Settings
 
-    Tabs.Player:AddSlider("player_speed", {
-        Title = "Speed",
-        Description = "Configuration for player speed.",
-        Default = 0,
-        Min = 0,
-        Max = 100,
-        Rounding = 3
-    })
-
-    Tabs.Player:AddSlider("player_jumping", {
-        Title = "Power",
-        Description = "Configuration for jump power.",
-        Default = 0,
-        Min = 0,
-        Max = 100,
-        Rounding = 3
-    })
+local Settings = Window.Tabs.Settings:AddRightGroupbox("Settings") do
+	Settings:AddToggle('settings_list', { Text = 'Keybind List', Default = false, Tooltip = 'Enables a list showing active keybinds.', Callback = function(v) Library.KeybindFrame.Visible = v end})
+	Settings:AddToggle('settings_watermark', { Text = 'Watermark', Default = true, Tooltip = 'Enables a watermark', Callback = function(v) Library:SetWatermarkVisibility(v) end})
+	Settings:AddButton('Unload', function() Library:Unload() end)
+	Settings:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+	Library.ToggleKeybind = Options.MenuKeybind
 end
 
-Tabs.Combat:AddSection("Hitbox Manipulation") do
-    Tabs.Combat:AddToggle("combat_hitbox", {Title = "Enabled", Description = "May not work for some games.", Default = false })
-end
+--// Watermark
 
-SaveManager:SetLibrary(Fluent) InterfaceManager:SetLibrary(Fluent) SaveManager:IgnoreThemeSettings() SaveManager:SetIgnoreIndexes({}) InterfaceManager:SetFolder("Lunar") SaveManager:SetFolder("Lunar/" .. tostring(game.PlaceId)) InterfaceManager:BuildInterfaceSection(Tabs.Settings) SaveManager:BuildConfigSection(Tabs.Settings) Window:SelectTab(1) SaveManager:LoadAutoloadConfig()
+Library:SetWatermarkVisibility(true)
+
+
+local WatermarkConnection = game:GetService('RunService').RenderStepped:Connect(function(delta)
+
+    Library:SetWatermark(('Lunar.dev | Jailbird | %s fps | %s ms'):format(
+        math.floor(1 / delta),
+        math.floor(game:GetService('Stats').Network.ServerStatsItem['Data Ping']:GetValue())
+    ));
+end);
+
+--// Setting up unload function
+Library:OnUnload(function()
+    WatermarkConnection:Disconnect()
+	Library.KeybindFrame.Visible = false
+    Library.Unloaded = true
+end)
+
+
+--// Setting up ui settings
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+ThemeManager:SetFolder('Lunar/themes')
+ThemeManager:ApplyToTab(Window.Tabs.Settings)
+SaveManager:LoadAutoloadConfig()
+SaveManager:BuildConfigSection(Window.Tabs.Settings)
+SaveManager:SetFolder('Lunar/configs')
+SaveManager:IgnoreThemeSettings()
