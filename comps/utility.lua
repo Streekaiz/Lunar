f406bfe6eabb27ec13bb410ec83342a6a2cdcded
Streekaiz <110584665+Streekaiz@users.noreply.github.com>
2024-07-22 07:08:43 -0700

    Update utility.lua

--- comps/utility.lua
+++ comps/utility.lua
@@ -1,136 +1,265 @@
---// Variables
-local Players = game:GetService("Players")
-local LPlayer = Players.LocalPlayer
-local Team = LPlayer.Team
-local Mouse = game:GetService("UserInputService"):GetMouseLocation()
+-- Services
+local InputService, TeleportService, RunService, Workspace, Lighting, Players, HttpService, StarterGui, ReplicatedStorage, TweenService  = game:GetService("UserInputService"), game:GetService("TeleportService"), game:GetService("RunService"), game:GetService("Workspace"), game:GetService("Lighting"), game:GetService("Players"), game:GetService("HttpService"), game:GetService("StarterGui"), game:GetService("ReplicatedStorage"), game:GetService("TweenService")
+local LocalPlayer = Players.LocalPlayer
+local Mouse, Camera = LocalPlayer:GetMouse(), Workspace.Camera
+local Library = {Connections = {}, Drawings = {}}
 
-
-local Library = {
-    Connections = {},
-    Drawings = {},
-    HitSounds = {
-        Rust = 5043539486,
-        CSGO = 8679627751,
-        TF2 = 5650646664,
-        Neverlose = 8679627751,
-        Gamesense = 4753603610,
-    }
-}
-
-function Library:Connect(Connection, Function)
-    local connection = Connection:Connect(Function)
-    table.insert(Library.Connections, connection)
-    return connection
-end
-
-function Library:Draw(Object, Properties)
-    local Drawing = Drawing.new(Object)
-
-    for _, v in next, Properties do
-        Drawing[_] = v
+do 
+    function Library:Connection(Type, Callback)
+        local Connection = Type:Connect(Callback)
+        Library.Connections[#Library.Connections + 1] = Connection
+        --
+        return Connection
     end
-
-    table.insert(Library.Drawings)
-    return Drawing
-end
-
-function Library:Unload()
-    for _, v in next, Library.Connections do
-        v:Disconnect()
-    end
-
-    for _, v in next, Library.Drawings do
-        v:Remove()
-    end
-end
-
-function Library:isFriendly(Player)
-    return Player.Team == Team
-end
-
-function Library:isAlive(Player)
-    if Player.Character:FindFirstChildOfClass("Humanoid") then
-        if Player.Character:FindFirstChildOfClass("Humanoid").Health > 0 then
-            return true
+    --
+    function Library:Drawing(type, prop)
+        local obj = Drawing.new(type)
+        --
+        if prop then
+            for i,v in next, prop do
+                obj[i] = v
+            end
         end
+        --
+        table.insert(Library.Drawings, obj)
+        return obj  
     end
-    return false
-end
+    --
+    function Library:GetStatus(Player)
+        local Alive = false
+        local Character
 
-function Library:getTool(Player)
-    if Library:isAlive(Player) then 
-        if Player.Character:FindFirstChildWhichIsA("Tool") then
-            return Player.Character:FindFirstChildWhichIsA("Tool").Name
-        end
-    end
-end
-
-function Library:getDistance(Player)
-    if Library:isAlive(Player) and Library:isAlive(LPlayer) then
-        return math.round((Player.Character.HumanoidRootPart.Position - LPlayer.Character.HumanoidRootPart.Position).Magnitude)
-    end
-
-    --[[
-    outdated system; use distancefromplayer or whatever (change later)
-    ]]
-
-    return 0
-end
-
-function Library:convertPercentage(Percentage)
-    local Random = math.random(0, 100)
-    if Random < Percentage then
-        return true
-    end
-    return false
-end
-
-function Library:Ray(Part, Orgin, Ignore, Distance)
-    local Ignore = Ignore or {}
-    local Distance = Distance or 5000
-
-    local Cast = Ray.new(Orgin, (Part.Position - Orgin).Unit * Distance)
-    local Hit = workspace:FindPartOnRayWithIgnoreList(Cast, Ignore)
-
-    return (Hit and Hit:IsADescendantOf(Part.Parent)) == true, Hit
-end
-
-function Library:getClosestPlayerToMouse(Friendly, Distance, Visible)
-    local CurrentMouseDistance = 1000
-    local Target
-
-    for _, v in next, game:GetService("Players"):GetPlayers() do
-        
-        --// Simple validation checks
-        if v == LPlayer then return end
-        if not Library:isAlive(v) and not Library:isAlive(LPlayer) then return end
-        if Friendly and Library:isFriendly(v) then return end 
-        if Distance[1] == true and Library:getDistance(v) <= Distance[2] then return end
-
-        local isVisible = true
-
-        if Visible[1] then -- idk how to use raycasts????????
-            local Results, Success = Library:Ray(v.Character[Visible[2]], LPlayer.Character[Visible[3]], {}, Distance[2])
-            if Success == false or Results.Hit ~= v.Character[Visible[2]] then
-                isVisible = false
+        if Player.Character and Player.Character:FindFirstChild("HumanoidRootPart") then
+            if Player.Character:FindFirstChildWhichIsA("Humanoid") then 
+                if Player.Character:FindFirstChildWhichIsA("Humanoid").Health > 0 then
+                    Alive = true
+                end
             end
         end
 
-        if not isVisible then return end
-        
-        local Success, TargetPosition = workspace.CurrentCamera:WorldToViewportPoint(v.Character[Visible[2]])
+        return Library:GetTeamStatus(LocalPlayer, Player), Alive, Player.Character
+    end
+    function Library:ChangeProperties(Object, Properties)
+        for i, v in next, Properties do 
+            Object[i] = v 
+        end
 
-        if not Success then return end
-
-        local TargetMouseDistance = Vector2.new(TargetPosition.X - Mouse.X, TargetPosition.Y - TargetPosition.Y)
-
-        if CurrentMouseDistance > TargetMouseDistance then
-            CurrentMouseDistance = TargetMouseDistance
-            Target = v 
+        return Object
+    end
+    --
+    function Library:GetBodyParts(Character, RootPart, Indexes, Hitboxes)
+        local Parts = {}
+        local Hitboxes = Hitboxes or {"Head", "Torso", "Arms", "Legs"}
+        --
+        for Index, Part in pairs(Character:GetChildren()) do
+            if Part:IsA("BasePart") and Part ~= RootPart then
+                if table.find(Hitboxes, "Head") and Part.Name:lower():find("head") then
+                    Parts[Indexes and Part.Name or #Parts + 1] = Part
+                elseif table.find(Hitboxes, "Torso") and Part.Name:lower():find("torso") then
+                    Parts[Indexes and Part.Name or #Parts + 1] = Part
+                elseif table.find(Hitboxes, "Arms") and Part.Name:lower():find("arm") then
+                    Parts[Indexes and Part.Name or #Parts + 1] = Part
+                elseif table.find(Hitboxes, "Legs") and Part.Name:lower():find("leg") then
+                    Parts[Indexes and Part.Name or #Parts + 1] = Part
+                elseif (table.find(Hitboxes, "Arms") and Part.Name:lower():find("hand")) or (table.find(Hitboxes, "Legs ") and Part.Name:lower():find("foot")) then
+                    Parts[Indexes and Part.Name or #Parts + 1] = Part
+                end
+            end
+        end
+        --
+        return Parts
+    end
+    --
+    function Library:GetClosestPlayer()
+        local shortestDistance = math.huge
+        --  
+        local closestPlayer
+        for _, Player in pairs(Players:GetPlayers()) do
+            if Player ~= LocalPlayer and Library:GetPlayerStatus(Player) then
+                local pos,OnScreen = Camera:WorldToViewportPoint(Player.Character.HumanoidRootPart.Position)
+                local magnitude = (Vector2.new(pos.X, pos.Y) - Vector2.new(Mouse.X, Mouse.Y)).magnitude
+                --
+                if magnitude < shortestDistance and OnScreen then
+                        closestPlayer = Player
+                        shortestDistance = magnitude
+                    end
+                end
+            end 
+        return closestPlayer
+    end
+    -- 
+    function Library:RayCast(Part, Origin, Ignore, Distance)
+        local Ignore = Ignore or {}
+        local Distance = Distance or 5000
+        --
+        local Cast = Ray.new(Origin, (Part.Position - Origin).Unit * Distance)
+        local Hit = Workspace:FindPartOnRayWithIgnoreList(Cast, Ignore)
+        --
+        return (Hit and Hit:IsDescendantOf(Part.Parent)) == true, Hit
+    end
+    --
+    function Library:GetOrigin(Origin)
+        if Origin == "Head" then
+            local Object, Humanoid, RootPart = Library:ValidateClient(LocalPlayer)
+            local Head = Object:FindFirstChild("Head")
+            --
+            if Head and Head:IsA("RootPart") then
+                return Head.CFrame.Position
+            end
+        elseif Origin == "Torso" then
+            local Object, Humanoid, RootPart = Library:ValidateClient(LocalPlayer)
+            --
+            if RootPart then
+                return RootPart.CFrame.Position
+            end
+        end
+        --
+        return Workspace.CurrentCamera.CFrame.Position
+    end
+    --
+    function Library:GetTeamStatus(Player1, Player2)
+        return (Player1.Team ~= Player2.Team)
+    end
+    --
+    function Library:GetPlayerStatus(Player: Instance)
+        if not Player then Player = LocalPlayer end
+        return Player.Character and Player.Character:FindFirstChild("Humanoid") and Player.Character.Humanoid.Health > 0 and true or false
+    end 
+    --
+    function Library:GetClosestPart(Player: Instance, List: Table)
+        local shortestDistance = math.huge
+        local closestPart
+        if Library:GetPlayerStatus(Player) then
+            for _, Value in pairs(Player.Character:GetChildren()) do
+                if Value:IsA("BasePart") then 
+                    local pos = Camera:WorldToViewportPoint(Value.Position)
+                    local magnitude = (Vector2.new(pos.X, pos.Y) - Vector2.new(Mouse.X, Mouse.Y + 36)).magnitude
+                        if magnitude < shortestDistance and table.find(List, Value) then
+                            closestPart = Value
+                            shortestDistance = magnitude
+                        end
+                    end
+                end 
+            return closestPart
+        end
+    end 
+    --
+    function Library:RandomChance(Percentage: Int)
+        local Chance = Percentage
+        --
+        if math.random(1,100) <= Chance then
+            return true 
+        else
+            return false
         end
     end
+    --
+    function Library:CloneCharacter(Player: Instance, Color: Color3, Material: Enum, Transparency: Int)
+        for i,v in pairs(Player.Character:GetChildren()) do 
+            if v:IsA("BasePart") and v.Name ~= "HumanoidRootPart" then 
+                local ClonedPart = Instance("Part")
+                ClonedPart.Anchored = true 
+                ClonedPart.CanCollide = false 
+                ClonedPart.Position = v.Position
+                ClonedPart.Parent = Workspace.Terrain 
+                ClonedPart.Material = Enum.Material[Material]
+                ClonedPart.Shape = Enum.PartType.Block 
+                ClonedPart.Transparency = Transparency 
+                ClonedPart.Color = Color
+                ClonedPart.Size = v.Size + Vector3.new(0.01,0.01,0.01)
+                ClonedPart.Name = v.Name
+                ClonedPart.Rotation = v.Rotation
+            end 
+        end
+    end 
+    --
+    function Library:CalculateAbsolutePosition(Player: Instance)
+        if Library:GetPlayerStatus(Player) then
+            local root = Player.Character.HumanoidRootPart
+            local character = Player.Character 
+            --
+            local currentPosition = root.Position
+            local currentTime = tick() 
+            --
+            task.wait()
+            --
+            local newPosition = root.Position
+            local newTime = tick()
+            --
+            local distanceTraveled = (newPosition - currentPosition) 
+            --
+            local timeInterval = newTime - currentTime
+            local velocity = distanceTraveled / timeInterval
+            currentPosition = newPosition
+            currentTime = newTime
+            --
+            return velocity
+        end
+    end  
+    --
+    function Library:CreateBeam(Origin, End, Color1, Color2, Texture)
+        local BeamPart = Instance("Part", workspace)
+        BeamPart.Name = "BeamPart"
+        BeamPart.Transparency = 1
+        --
+        local Part = Instance("Part", BeamPart)
+        Part.Size = Vector3.new(1, 1, 1)
+        Part.Transparency = 1
+        Part.CanCollide = false
+        Part.CFrame = CFrame.new(Origin)
+        Part.Anchored = true
+        local Attachment = Instance("Attachment", Part)
+        local Part2 = Instance("Part", BeamPart)
+        Part2.Size = Vector3.new(1, 1, 1)
+        Part2.Transparency = 1
+        Part2.CanCollide = false
+        Part2.CFrame = CFrame.new(End)
+        Part2.Anchored = true
+        Part2.Color = Color3.fromRGB(255, 255, 255)
+        local Attachment2 = Instance("Attachment", Part2)
+        local Beam = Instance("Beam", Part)
+        Beam.FaceCamera = true
+        Beam.Color = ColorSequence.new{
+            ColorSequenceKeypoint.new(0.00, Color1),
+            ColorSequenceKeypoint.new(1, Color2),
+        }
+        Beam.Attachment0 = Attachment
+        Beam.Attachment1 = Attachment2
+        Beam.LightEmission = 6
+        Beam.LightInfluence = 1
+        Beam.Width0 = 1
+        Beam.Width1 = 0.6
+        Beam.Texture = "rbxassetid://".. Texture ..""
+        Beam.LightEmission = 1
+        Beam.LightInfluence = 1
+        Beam.TextureMode = Enum.TextureMode.Wrap 
+        Beam.TextureLength = 3 
+        Beam.TextureSpeed = 3
+        delay(1, function()
+        for i = 0.5, 1, 0.02 do
+            task.wait()
+            Beam.Transparency = NumberSequence.new(i)
+        end
+        Part:Destroy()
+        Part2:Destroy()
+        BeamPart:Destroy()
+        end)
+    end
+    --
+    function Library:GetTool() 
+        if LocalPlayer.Character and LocalPlayer.Character:FindFirstChildWhichIsA("Tool") and Library:GetPlayerStatus() then 
+            return LocalPlayer.Character:FindFirstChildWhichIsA("Tool") 
+        end 
+    end
 
-    return Target
+    function Library:Unload()
+        for i, v in next, Library.Connections do
+            v:Disconnect()
+        end
+        for i, v in next, Library.Connections do 
+            v:Remove()
+        end
+    end
 end
 
-return Library
+return Library, LocalPlayer, InputService, TeleportService, RunService, Lighting, Players, HttpService, ReplicatedStorage, TweenService
